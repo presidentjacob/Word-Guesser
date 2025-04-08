0e94b8cebab2f84eb3bbfae889e4e932ee86b733
Jacob Yepez <jacobyepez04@gmail.com>
2025-04-08 13:32:16 -0600

    Added logging to project

--- word-guesser.py
+++ word-guesser.py
@@ -1,11 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelnames)s - %(message)s')
 
 def scraper(url):
+    logging.debug('Attempting scraper')
     try:
         response = requests.get(url, timeout=10)
     except Exception as e:
-        print('Error: ', e)
+        logging.debug('Error: ', e)
         return None
 
     if response != 200:
