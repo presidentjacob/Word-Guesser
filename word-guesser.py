8ac438a96de1f806d38bffa2a21af364929abf0f
Jacob Yepez <jacobyepez04@gmail.com>
2025-04-08 13:35:48 -0600

    Create a soup and find the paragraph with all words in it

--- word-guesser.py
+++ word-guesser.py
@@ -5,6 +5,7 @@ import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelnames)s - %(message)s')
 
 def scraper(url):
+    # Attempt to get a response from the url, exit if no response
     logging.debug('Attempting scraper')
     try:
         response = requests.get(url, timeout=10)
@@ -14,6 +15,13 @@ def scraper(url):
 
     if response != 200:
         return None
+    
+    # Create a soup in lxml
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    # Find all the words
+    words = soup.find('p')
+
 
 def main():
     url = 'https://www.ef.edu/english-resources/english-vocabulary/top-1000-words/'
