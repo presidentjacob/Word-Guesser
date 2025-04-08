e24e392b51e615179fc459b0c8aba70eedccc15f
Jacob Yepez <jacobyepez04@gmail.com>
2025-04-08 17:24:53 -0600

    Added comments

--- word-guesser.py
+++ word-guesser.py
@@ -49,16 +49,19 @@ def game():
     sheet = wb.active
     word_place = random.randint(1, 1000)
 
+    # Get the random word
     word = sheet[f'A{word_place}'].value
     length = len(word)
 
-    # Begin the game
     wrong_guesses = 0
 
+    # Setup a win condition
     right_letters = []
     game_won = False
     
+    # Start the game
     while wrong_guesses < 5 and game_won == False:
+        # Print game
         while True:
             for i in range(length):
                 if word[i] in right_letters:
@@ -66,16 +69,22 @@ def game():
                 else:
                     print('_ ', end='')
             print()
+            
+            # Ensure that only 1 character is entered
             guess = input("Enter in a single character: ")
             if len(guess) == 1:
                 break
             print("Try again, please only one character.")
+
+        # Add letter if it is in word and not in right_letters
         if guess in word and guess not in right_letters:
+            # Append letter to right_letters
             right_letters.append(guess)
             print()
+        # IF word is in right_letters, continue
         elif guess in right_letters:
             print("You already tried this letter!\n")
-            continue
+        # If wrong letter, add to wrong_guesses
         else:
             print("Sorry! That is not the correct letter!\n")
             wrong_guesses += 1
