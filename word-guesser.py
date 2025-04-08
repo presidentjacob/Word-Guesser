5aa029cc9cf0956c53a6c5f2ddb9d47874c825a2
Jacob Yepez <jacobyepez04@gmail.com>
2025-04-08 13:30:00 -0600

    Added timer and a response check

--- word-guesser.py
+++ word-guesser.py
@@ -3,10 +3,12 @@ from bs4 import BeautifulSoup
 
 def scraper(url):
     try:
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'lxml')
+        response = requests.get(url, timeout=10)
     except Exception as e:
         print('Error: ', e)
+        return None
+
+    if response != 200:
         return None
 
 def main():
